5a8facedbbd3683bbb043ca10f01300881e685e1
沈超 <shenchao3@xiaomi.com>
2017-06-15 19:51:19 +0800

    no

--- src/main/resources/thrift/User.thrift
+++ src/main/resources/thrift/User.thrift
@@ -1,25 +1,13 @@
 /**定义包名 */
 namespace java sc.learn.manage.service
 
-
-/**定义请求的传输对象*/
-struct TUserRequest{
+struct TUser{
 1:required string name,
-2:required i32 age,
-3:required i64 birthday,
-4:required string remark
-}
-
-/**定义返回的传输对象*/
-struct TUserResponse{
-1:required string name,
-2:required i32 age,
-3:required i64 birthday,
-4:required string remark
+2:required string email,
+3:required string password
 }
 
 /**定义两个接口方法*/
 service TUserService{
-	void save(1:TUserRequest user)
-	TUserResponse findByName(1:string name);
+	void save(1:TUser user)
 }
