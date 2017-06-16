6d2847ab50d8dd50349d55823c8355d8f0b1dacd
沈超 <shenchao3@xiaomi.com>
2017-06-16 19:41:33 +0800

    no

--- src/test/java/sc/learn/test/common/TestHadoop.java
+++ src/test/java/sc/learn/test/common/TestHadoop.java
@@ -16,9 +16,9 @@ public class TestHadoop {
 		FileSystem hdfs=FileSystem.get(URI.create("hdfs://localhost:9000/"),conf);
 		FileSystem local=FileSystem.getLocal(conf);
 		
-//		for(FileStatus fileStatus:local.listStatus(new Path("/home/shenchao3"))){
-//			System.out.println(fileStatus.getPath().getName());
-//		}
+		for(FileStatus fileStatus:local.listStatus(new Path("/home/shenchao3"))){
+			System.out.println(fileStatus.getPath().getName());
+		}
 		for(FileStatus fileStatus:hdfs.listStatus(new Path("/"))){
 			System.out.println(fileStatus.getPath().getName());
 		}
