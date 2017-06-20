a3cbe41c877b272505fc854162301eda8f7d97b2
沈超 <shenchao3@xiaomi.com>
2017-06-20 19:20:47 +0800

    no

--- src/test/java/sc/learn/test/common/TestBlockIO.java
+++ src/test/java/sc/learn/test/common/TestBlockIO.java
@@ -10,31 +10,35 @@ public class TestBlockIO {
 
 	@Test
 	public void testServer() throws Exception {
-		DatagramSocket socket = new DatagramSocket(8800);
-		boolean isFirst=true;
-		while (true) {
-			if(isFirst){
-				Thread.sleep(10000);
-				isFirst=false;
+		try(DatagramSocket socket = new DatagramSocket(8800);){
+			boolean isFirst=true;
+			while (true) {
+				if(isFirst){
+					Thread.sleep(10000);
+					isFirst=false;
+				}
+				byte[] data = new byte[1024];
+				DatagramPacket packet = new DatagramPacket(data, data.length);
+				System.out.println("****服务器端已经启动，等待客户端发送信息");
+				socket.receive(packet);
+				String info = new String(data, 0, packet.getLength());
+				System.out.println("我是服务器，客户端说：" + info);
 			}
-			byte[] data = new byte[1024];
-			DatagramPacket packet = new DatagramPacket(data, data.length);
-			System.out.println("****服务器端已经启动，等待客户端发送信息");
-			socket.receive(packet);
-			String info = new String(data, 0, packet.getLength());
-			System.out.println("我是服务器，客户端说：" + info);
 		}
+		
 	}
 
 	@Test
 	public void testClient() throws Exception {
 		InetAddress address = InetAddress.getByName("localhost");
 		int port = 8800;
-		DatagramSocket socket = new DatagramSocket();
-		for(int i=0;i<1000000;i++){
-			byte[] data = ("用户名：admin"+(i+1)+"; 密码:123").getBytes();
-			DatagramPacket packet = new DatagramPacket(data, data.length, address, port);
-			socket.send(packet);
+		try(DatagramSocket socket = new DatagramSocket();){
+			for(int i=0;i<1000000;i++){
+				byte[] data = ("用户名：admin"+(i+1)+"; 密码:123").getBytes();
+				DatagramPacket packet = new DatagramPacket(data, data.length, address, port);
+				socket.send(packet);
+			}
 		}
+		
 	}
 }
