fe012d2127434c6c3faa301fe6f44f6d5ee5515c
沈超 <shenchao3@xiaomi.com>
2017-06-26 17:30:37 +0800

    no

---
+++ src/main/java/sc/learn/manage/aop/BizParamPrintAspect.java
@@ -0,0 +1,34 @@
+package sc.learn.manage.aop;
+
+import java.util.Arrays;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Aspect  
+public class BizParamPrintAspect {
+	
+	private static final Logger LOGGER=LoggerFactory.getLogger(BizParamPrintAspect.class);
+	
+    @Pointcut("execution(* sc.learn.*.biz.*.*(..))")  
+    private void anyMethod(){}//定义一个切入点  
+      
+    @Around("anyMethod()")  
+    public Object doBasicProfiling(ProceedingJoinPoint pjp) throws Throwable{
+    	LOGGER.debug("执行方法:{}",pjp.getSignature().getName());
+    	LOGGER.debug("方法参数:{}",Arrays.toString(pjp.getArgs()));
+		try {
+			Object object = pjp.proceed();
+			LOGGER.debug("方法结果:{}",object);
+	        LOGGER.debug("方法参数:{}",Arrays.toString(pjp.getArgs()));
+	        return object;
+		} catch (Throwable e) {
+			LOGGER.debug("方法异常:{}",e.getLocalizedMessage());
+			throw e;
+		}
+    }  
+}  
